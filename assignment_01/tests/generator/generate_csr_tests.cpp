6a4fc97e1b85aa7146170d49f1bdf705f1092033
2026csm1005-gif <2026csm1005@iitrpr.ac.in>
2026-08-05 07:16:07 +0530

    Add CSR test cases
    - Added Test Cases 1 to 8 manually.
    - Added Test Cases 9 and 10 using Utility Generator for CSR.

---
+++ assignment_01/tests/generator/generate_csr_tests.cpp
@@ -0,0 +1,43 @@
+#include <fstream>
+#include <iostream>
+#include <set>
+#include <vector>
+
+using namespace std;
+
+void generateUndirectedGraph(const string &fileName, int vertices, int neighboursPerVertex)
+{
+    vector<set<int>> adjacency(vertices);
+    for (int i = 0; i < vertices; i++)
+    {
+        for (int j = 1; j <= neighboursPerVertex; j++)
+        {
+            int neighbour = (i + j) % vertices;
+            adjacency[i].insert(neighbour);
+            adjacency[neighbour].insert(i);
+        }
+    }
+    int edgeCount = 0;
+    for (const auto &neighbors : adjacency)
+        edgeCount += neighbors.size();
+
+    edgeCount /= 2;
+    ofstream file(fileName);
+    file << vertices << " " << edgeCount << "\n";
+    for (int i = 0; i < vertices; i++)
+    {
+        file << i << " " << adjacency[i].size() << " ";
+        for (int neighbor : adjacency[i])
+            file << neighbor << " ";
+        file << "\n";
+    }
+    file.close();
+    cout << fileName << " generated successfully.\n";
+}
+
+int main()
+{
+    generateUndirectedGraph("tests/csr/csr_test_09.txt", 10, 3);
+    generateUndirectedGraph("tests/csr/csr_test_10.txt", 100, 3);
+    return 0;
+}
